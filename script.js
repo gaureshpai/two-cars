4c723ee9e76e266b04b79a7f3483752a083a5b61
Jnanesh <jtuluve@gmail.com>
2025-01-10 19:19:27 +0530

    Optimized and added pause option

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// global variables
 let scoreEl = document.getElementById("score");
+let score = 0;
+let active = true;
+let speedFactor = 1;
+let height = 650;
 
+//classes
 class Car {
   /**
    *
@@ -18,6 +24,7 @@ class Car {
   }
 
   mov() {
+    if (!active) return;
     if (
       this.animate &&
       this.animate.currentTime < this.animate.effect.getTiming().duration - 80
@@ -70,46 +77,86 @@ class Tracks {
     this.car = car;
     this.t1Objects = new Set();
     this.t2Objects = new Set();
-    this.active = true;
   }
+
   start() {
     this.addObject();
   }
 
   stop() {
-    this.active = false;
-    this.t1Objects.forEach((obj) => obj.stop());
-    this.t2Objects.forEach((obj) => obj.stop());
+    active = false;
+    clearTimeout(this.timeout);
+
+    this.t1Objects.forEach((obj) => {
+      obj.animation?.pause();
+    });
+    this.t2Objects.forEach((obj) => {
+      obj.animation?.pause();
+    });
+  }
+
+  pause() {
+    active = false;
+    clearTimeout(this.timeout);
+    this.t1Objects.forEach((obj) => {
+      obj.pause();
+    });
+    this.t2Objects.forEach((obj) => {
+      obj.pause();
+    });
+  }
+
+  resume() {
+    active = true;
+
+    this.t1Objects.forEach((obj) => {
+      obj.resume();
+    });
+    this.t2Objects.forEach((obj) => {
+      obj.resume();
+    });
+
+    this.addObject();
   }
 
   addObject() {
-    let whichTrack = random() > 0.5 ? true : false;
-    if (whichTrack) {
-      let whichObj = random() > 0.5 ? true : false;
-      let objel = document.createElement("div");
-      let obj = whichObj
-        ? new Stone(objel, this.car, 20)
-        : new Coin(objel, this.car, 20);
-      this.t1Objects.add(obj);
-      this.track1.appendChild(objel);
-    } else {
-      let whichObj = random() > 0.5 ? true : false;
-      let objel = document.createElement("div");
-      let obj = whichObj
-        ? new Stone(objel, this.car, 110)
-        : new Coin(objel, this.car, 110);
-      this.t2Objects.add(obj);
-      this.track2.appendChild(objel);
-    }
-    setTimeout(() => {
-      if (!this.active) return;
+    this.timeout = setTimeout(() => {
+      if (!active) return;
+      let whichTrack = random() > 0.5 ? true : false;
+      if (whichTrack) {
+        let whichObj = random() > 0.5 ? true : false;
+        let objel = document.createElement("div");
+        let obj = whichObj
+          ? new Stone(objel, this.car, 20)
+          : new Coin(objel, this.car, 20);
+        this.t1Objects.add(obj);
+        this.track1.appendChild(objel);
+      } else {
+        let whichObj = random() > 0.5 ? true : false;
+        let objel = document.createElement("div");
+        let obj = whichObj
+          ? new Stone(objel, this.car, 110)
+          : new Coin(objel, this.car, 110);
+        this.t2Objects.add(obj);
+        this.track2.appendChild(objel);
+      }
       this.addObject();
-    }, 2000 + random() * 1000);
+    }, 2000);
   }
 
   removePassedObjs() {
-    this.t1Objects = this.t1Objects.filter((obj) => obj.top > 650);
-    this.t2Objects = this.t2Objects.filter((obj) => obj.top > 650);
+    this.t1Objects.forEach((obj) => {
+      if (obj.el.offsetTop > 650) {
+        this.t1Objects.delete(obj);
+        obj.el.remove();
+      }
+    });
+    this.t2Objects.forEach((obj) => {
+      if (obj.el.offsetTop > 650) {
+        this.t2Objects.delete(obj);
+        obj.el.remove();
+      }
+    });
   }
 }
 
@@ -123,24 +170,30 @@ class Obj {
   constructor(el, car, left) {
     this.el = el;
     this.car = car;
-    this.active = true;
-    this.top = -50;
     this.el.top = this.top + "px";
     this.width = 50;
     this.left = left;
   }
 
   mov() {
-    if (!this.active) return;
-    this.top += 10;
-    this.el.animate([{ top: this.top + "px" }], {
-      duration: 80,
+    if (!active) return;
+    this.animation = this.el.animate([{ top: height + "px" }], {
+      easing: "linear",
+      duration: 10000,
       fill: "forwards",
     });
   }
 
-  stop() {
-    this.active = false;
+  pause() {
+    this.animation?.pause();
+  }
+
+  resume() {
+    this.checkInterval = setInterval(() => {
+      if (!active) return clearInterval(this.checkInterval);
+      this.checkCollide();
+    }, 10);
+    this.animation?.play();
   }
 }
 
@@ -148,32 +201,26 @@ class Stone extends Obj {
   constructor(el, car, left) {
     super(el, car, left);
     this.el.id = "stone";
-    this.active = true;
-    this.interval = setInterval(() => {
-      if (!this.active) {
-        clearInterval(this.Interval);
-      }
-      this.mov();
-    }, 100);
+    this.mov();
 
-    this.interval = setInterval(() => {
+    this.checkInterval = setInterval(() => {
+      if (!active) return clearInterval(this.checkInterval);
       this.checkCollide();
     }, 10);
   }
 
   checkCollide() {
     if (
-      this.el.offsetTop > 480 &&
-      this.el.offsetTop < 625 &&
+      this.el.offsetTop > 490 &&
+      this.el.offsetTop < 630 &&
       this.car.el.offsetLeft > this.left - 50 &&
       this.car.el.offsetLeft < this.left + 50
     ) {
-      this.active = false;
-      clearInterval(this.interval);
+      active = false;
       globalStop();
       this.el.animate([{ scale: 0.9 }], {
         direction: "alternate",
-        duration: 1000,
+        duration: 500,
         iterations: Infinity,
       });
       return true;
@@ -185,39 +232,35 @@ class Coin extends Obj {
   constructor(el, car, left) {
     super(el, car, left);
     this.el.id = "coin";
-    this.active = true;
-    this.interval1 = setInterval(() => {
-      if (!this.active) {
-        clearInterval(this.interval1);
-      }
-      this.mov();
-    }, 100);
+    this.mov();
 
-    this.interval2 = setInterval(() => {
-      if (!this.active) {
-        clearInterval(this.interval2);
+    this.interval = setInterval(() => {
+      if (!active) {
+        clearInterval(this.interval);
       }
       this.checkCollide();
     }, 10);
   }
 
   checkCollide() {
+    //collect coin
     if (
       this.el.offsetTop > 475 &&
       this.el.offsetTop < 620 &&
       this.car.el.offsetLeft > this.left - 50 &&
       this.car.el.offsetLeft < this.left + 50
     ) {
-      this.active = false;
       this.el.remove();
-      scoreEl.innerText = parseInt(scoreEl.innerText) + 1;
+      scoreEl.innerText = ++score;
       clearInterval(this.interval);
       return true;
     }
-    if (this.el.offsetTop > 610) {
+
+    // if coin passes, game end
+    if (this.el.offsetTop > 632) {
       this.el.animate([{ scale: 1.1 }], {
         direction: "alternate",
-        duration: 1000,
+        duration: 500,
         iterations: Infinity,
       });
       globalStop();
@@ -225,6 +268,7 @@ class Coin extends Obj {
   }
 }
 
+// implementation
 let llTrack = document.getElementById("llTrack");
 let lrTrack = document.getElementById("lrTrack");
 let rlTrack = document.getElementById("rlTrack");
@@ -232,6 +276,12 @@ let rrTrack = document.getElementById("rrTrack");
 
 let car1 = new Car(document.getElementById("car1"), 20, 110, true);
 let car2 = new Car(document.getElementById("car2"), 20, 110, true);
+car1.addListener("a");
+car1.addListener("A");
+car1.addListener("ArrowLeft");
+car2.addListener("d");
+car2.addListener("D");
+car2.addListener("ArrowRight");
 let lTracks, rTracks;
 
 function globalStop() {
@@ -242,7 +292,10 @@ function globalStop() {
 }
 
 function start() {
+  score = 0;
+  speedFactor = 1;
   globalStop();
+  active = true;
   car1.reset();
   car2.reset();
   lTracks = new Tracks(llTrack, lrTrack, car1);
@@ -252,12 +305,6 @@ function start() {
   rlTrack.innerHTML = "";
   rrTrack.innerHTML = "";
   scoreEl.innerText = 0;
-  car1.addListener("a");
-  car1.addListener("A");
-  car1.addListener("ArrowLeft");
-  car2.addListener("d");
-  car2.addListener("D");
-  car2.addListener("ArrowRight");
 
   lTracks.start();
   rTracks.start();
@@ -270,6 +317,14 @@ function random() {
 window.onkeydown = (e) => {
   if (e.key == "r" || e.key == "R") {
     start();
+  } else if (e.key == "p" || e.key == "P") {
+    if (active) {
+      lTracks?.pause();
+      rTracks?.pause();
+    } else {
+      lTracks?.resume();
+      rTracks?.resume();
+    }
   }
 };
 
